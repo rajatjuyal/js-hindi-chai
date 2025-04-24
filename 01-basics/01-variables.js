64036c1ee84957174ef114abb11223c9ae896737
monalisa octocat <39550821+rajatjuyal@users.noreply.github.com>
2025-04-24 09:04:21 +0000

    added js files of day 1 and day 2 with updates to devcontainer.json file

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountID = 123456
+let accountEmail = "something@gmail.com"
+var accountPassword = "12345"
+accountCity = "noida"
+
+
+// accountID  = 123
+accountPassword="112233"
+accountCity = "uttrakhand"
+
+console.log(accountID);
+console.table([accountPassword,accountEmail])
+console.table([accountPassword,accountEmail,accountCity])
---
+++ 01-basics/02-dataTypes.js
@@ -0,0 +1,11 @@
+
+/*
+number = 2 power 53
+bigint - bigger number
+boolean - true or false
+undefined - value is not assigned like if we do let state;
+null - standalone value
+string - ant string like let name  = "rajat". try to keep double quotes.
+typeof  - find type of datatype of a value like console.log(typeof name)
+
+*/
