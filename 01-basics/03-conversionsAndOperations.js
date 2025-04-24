c18d5f6fecd1d0a4c659405c3e5c42fa8cab29fe
monalisa octocat <39550821+rajatjuyal@users.noreply.github.com>
2025-04-24 10:13:31 +0000

    operations and pre-post fix

--- 01-basics/03-conversionsAndOperations.js
+++ 01-basics/03-conversionsAndOperations.js
@@ -14,8 +14,8 @@ let valueinumber1 = Number(score1)
 
 let score2 = null 
 let valueinumber2 = Number(score2)
-console.log(valueinumber2)
-console.log(typeof valueinumber2)
+// console.log(valueinumber2)
+// console.log(typeof valueinumber2)
 
 
 let score3 = undefined // gives NaN just like a alphanumeric input
@@ -41,3 +41,16 @@ Boolean
 "someting" => true
 1 => true and 0 => false
 */
+
+
+//*************************OPERATIONS***************************** */
+
+// PREFIX AND POSTFIX
+let x=3
+let y=x++
+console.table([x,y]);
+
+let a= 3n
+let b= ++a
+console.table([a,b])
+
